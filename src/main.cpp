807cfa82e9eade0f8a11a9c5f362f80fe0d7aee6
gumernus <gumernusbussines@gmail.com>
2023-07-31 21:45:56 +0200

    deleted old code and replaced it with a function

--- src/main.cpp
+++ src/main.cpp
@@ -39,25 +39,8 @@ int main () {
 	/* Open database */
 	rc = sqlite3_open("todo.db", &db);
 
-	if (rc) {
-		std::cerr << "Can't open database\n";
-		return 0;
-	} else { 
-		std::cout << "Database opened sucefully\n"; 
-	}
-
-	/* Make SQL statement */
-	sql = "CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY, task TEXT, completed INTEGER);";
-
-	/* Execute the statement */
-	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-
-	if (rc != SQLITE_OK) {
-    	std::cerr << "Table creation failed: " << &zErrMsg << '\n';
-	    sqlite3_free(zErrMsg);
-	} else {
-		std::cout << "Statement executed successfully\n";
-	}
+	/* Check if the table exists. If not create it */
+	execute_db_statement("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY, task TEXT, completed INTEGER);", rc, db);
 
 //	execute_db_statement("INSERT INTO todo (task, completed) VALUES ('Buy groceries', 0);", rc, db);
 
