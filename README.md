c5e82142175d5ce9bfb774a242869d9f67ca550a
gumernus <gumernusbussines@gmail.com>
2023-07-31 21:39:27 +0200

    added function to execute sql statements

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Compile with `make` and run with `./bin/sqlite_todo`
+
+You will need to have the sqlite3 library installed. This is how you install the library in debian distros:
+```
+sudo apt-get install libsqlite3-dev
+```
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,30 @@
 #include <sqlite3.h>
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+
+	for (int i = 0; i < argc; i++) {
+        std::cout << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << '\n';
+    }
+    std::cout << "-------------------\n";
+
 	return 0;
+
+}
+
+void execute_db_statement(const char *statement, int rc, sqlite3 *db) {
+
+	char *errMsg = 0;
+
+	/* Execute the statement */
+	rc = sqlite3_exec(db, statement, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+    	std::cerr << "Statement execution failed: " << &errMsg << '\n';
+		sqlite3_free(errMsg);
+	} else {
+		std::cout << "Statement executed successfully\n";
+	}
+
 }
 
 int main () {
@@ -35,6 +58,10 @@ int main () {
 	} else {
 		std::cout << "Statement executed successfully\n";
 	}
+
+//	execute_db_statement("INSERT INTO todo (task, completed) VALUES ('Buy groceries', 0);", rc, db);
+
+	execute_db_statement("SELECT * FROM todo;", rc, db);
 
 	sqlite3_close(db);
 	return 0;
