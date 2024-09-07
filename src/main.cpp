707efc0cecb39dbb2fe7e81548d56ce04862e6a0
gumernus <gumernusbussines@gmail.com>
2024-09-07 20:05:56 +0200

    added some commends

--- src/main.cpp
+++ src/main.cpp
@@ -37,11 +37,25 @@ int main () {
 	int rc = sqlite3_open("todo.db", &db);
 
 	/* Check if the table exists. If not create it */
-	execute_db_statement("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY, task TEXT, completed INTEGER);", rc, db);
+//	execute_db_statement("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY, task TEXT, completed INTEGER);", rc, db);
 
-//	execute_db_statement("INSERT INTO todo (task, completed) VALUES ('Buy groceries', 0);", rc, db);
+	/* Insert a task and that if its completed to the "todo" table */
+	/* execute_db_statement("INSERT INTO todo (task, completed) VALUES ('Clean windows', 1);", rc, db); */
 
+	/* See everything in the "todo" table */
 	execute_db_statement("SELECT * FROM todo;", rc, db);
+
+	/* Get the number of rows in the "todo" table */
+	/* execute_db_statement("SELECT COUNT(*) FROM todo;", rc, db); */
+
+	/* Get some info about the "todo" table */
+	/* execute_db_statement("PRAGMA table_info(todo);", rc, db); */
+
+	/* See every table in the db */
+	/* execute_db_statement("SELECT name FROM sqlite_master WHERE type='table';", rc, db); */
+
+	/* Change the "completed" state in a row in the "todo" table by id */
+	execute_db_statement("UPDATE todo SET completed = 1 WHERE id = 1;", rc, db);
 
 	sqlite3_close(db);
 	return 0;
